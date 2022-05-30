c15335a3840894825c4cf1d7143a5a72be6065c5
WhiskyAKM <patryk041103@gmail.com>
2022-05-30 22:07:10 +0200

    mobile and desktopview

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import './header.css'
+import {BrowserView, MobileView} from 'react-device-detect';
 
 function Header() {
   return (
-    <div className="header">
-        <div className="header-tiles">
-            <span className="header-title">TechNews</span>
+    <>
+      <BrowserView>
+        <div className="header">
+            <div className="header-tiles">
+                <span className="header-title">TechNews</span>
+            </div>
         </div>
-    </div>
+      </BrowserView>
+      <MobileView>
+        <div className="header">
+            <div className="header-tiles">
+                <span className="header-title" style={{"font-size": 50}}>TechNews</span>
+            </div>
+        </div>
+      </MobileView>
+    </>
   )
 }
 
