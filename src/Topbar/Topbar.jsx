faf9baf68f485ba6cb752b423a71bf69ab3d8973
WhiskyAKM <patryk041103@gmail.com>
2022-05-30 22:10:56 +0200

    mobile and desktopview

--- src/Topbar/Topbar.jsx
+++ src/Topbar/Topbar.jsx
@@ -1,26 +1,49 @@
 import React from "react";
 import "./topbar.css";
-
+import {BrowserView, MobileView} from 'react-device-detect';
 function Topbar()
 {
     return (
-        <div className="top">
-            <div className="top-left">
-                <i className="topIcon fa-brands fa-youtube"></i>
-                <i className="topIcon fa-brands fa-telegram"></i>
-                <i className="topIcon fa-brands fa-facebook"></i>
-            </div>
-            <div className="top-center">
-                <ul className="top-list">
-                    <li className="top-list-item">HOME</li>
-                    <li className="top-list-item">ABOUT</li>
-                    <li className="top-list-item">CONTACT</li>
-                </ul>
-            </div>
-            <div className="top-right">
-                <i className="topIcon fa-solid fa-magnifying-glass"></i>
-            </div>
-        </div>
+        <>
+            <BrowserView>
+                <div className="top">
+                    <div className="top-left">
+                        <i className="topIcon fa-brands fa-youtube"></i>
+                        <i className="topIcon fa-brands fa-telegram"></i>
+                        <i className="topIcon fa-brands fa-facebook"></i>
+                    </div>
+                    <div className="top-center">
+                        <ul className="top-list">
+                            <li className="top-list-item">HOME</li>
+                            <li className="top-list-item">ABOUT</li>
+                            <li className="top-list-item">CONTACT</li>
+                        </ul>
+                    </div>
+                    <div className="top-right">
+                        <i className="topIcon fa-solid fa-magnifying-glass"></i>
+                    </div>
+                </div>
+            </BrowserView>
+            <MobileView>
+                <div className="top" style={{"display" : "block"}}>
+                    <div className="top-left">
+                        <i className="topIcon fa-brands fa-youtube"></i>
+                        <i className="topIcon fa-brands fa-telegram"></i>
+                        <i className="topIcon fa-brands fa-facebook"></i>
+                    </div>
+                    <div className="top-center">
+                        <ul className="top-list">
+                            <li className="top-list-item">HOME</li>
+                            <li className="top-list-item">ABOUT</li>
+                            <li className="top-list-item">CONTACT</li>
+                        </ul>
+                    </div>
+                    <div className="top-right">
+                        <i className="topIcon fa-solid fa-magnifying-glass"></i>
+                    </div>
+                </div>
+            </MobileView>
+        </>
     );
 }
 
