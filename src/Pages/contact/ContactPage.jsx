3f2af5d00cabe3d85f21355f1da8769c6c09009d
WhiskyAKM <patryk041103@gmail.com>
2022-06-02 23:23:46 +0200

    fixed up look and added router

---
+++ src/Pages/contact/ContactPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserView, MobileView } from 'react-device-detect'
+
+function ContactPage() {
+  return (
+    <>
+        <BrowserView>
+        
+        </BrowserView>
+        <MobileView>
+            
+        </MobileView>
+    </>
+  )
+}
+
+export default ContactPage
--- src/comp/post/Post.jsx
+++ src/comp/post/Post.jsx
@@ -1,49 +1,98 @@
 import React from 'react'
+import { BrowserView, MobileView } from 'react-device-detect'
 import "./post.css"
 function Post() {
   return (
-    <div className="post">
-        <a href="https://ptfoplayer.github.io/w3/#/post/1">
-        <img className="post-image"
-            src={require("./pobrane.png")}
-            alt=""
-        />
-        </a>
-        <div className="post-info">
-            <div className="post-cats">
-                <span className="post-cat">Leak</span>
-                <span className="post-cat">News</span>
-            </div>
-            <a style = {{"textDecoration": "none"}}className="post-title" href="https://ptfoplayer.github.io/w3/#/post/1" >
-                <spam style = {{"textDecoration": "none"}} className = "post-title">
+    <>
+        <BrowserView>
+            <div className="post">
+                <a href="https://ptfoplayer.github.io/w3/#/post/1">
+                <img className="post-image"
+                    src={require("./pobrane.png")}
+                    alt=""
+                />
+                </a>
+                <div className="post-info">
+                    <div className="post-cats">
+                        <span className="post-cat">Leak</span>
+                        <span className="post-cat">News</span>
+                    </div>
+                    <a style = {{"textDecoration": "none"}}className="post-title" href="https://ptfoplayer.github.io/w3/#/post/1" >
+                        <spam style = {{"textDecoration": "none"}} className = "post-title">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        </spam>
+                    </a>
+                    <hr />
+                    <span className="post-date">
+                        January 1, 2020
+                    </span>
+                </div>
+                <p className="post-desc">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </spam>
-            </a>
-            <hr />
-            <span className="post-date">
-                January 1, 2020
-            </span>
-        </div>
-        <p className="post-desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quisquam, quidem.            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quisquam, quidem.            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quisquam, quidem.            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quisquam, quidem.
-        </p>
-    </div>
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.
+                </p>
+            </div>
+        </BrowserView>
+        <MobileView>
+        <div className="post" style={{"width" : "100%"}}>
+                <a href="https://ptfoplayer.github.io/w3/#/post/1">
+                <img className="post-image"
+                    src={require("./pobrane.png")}
+                    alt=""
+                />
+                </a>
+                <div className="post-info">
+                    <div className="post-cats">
+                        <span className="post-cat">Leak</span>
+                        <span className="post-cat">News</span>
+                    </div>
+                    <a style = {{"textDecoration": "none"}}className="post-title" href="https://ptfoplayer.github.io/w3/#/post/1" >
+                        <spam style = {{"textDecoration": "none"}} className = "post-title">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        </spam>
+                    </a>
+                    <hr />
+                    <span className="post-date">
+                        January 1, 2020
+                    </span>
+                </div>
+                <p className="post-desc">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.
+                </p>
+            </div>
+        </MobileView>
+    </>
   )
 }
 
