870875137723f96535532baad2589c6c17bb4898
WhiskyAKM <patryk041103@gmail.com>
2022-06-03 07:32:53 +0200

    router fixes

--- src/comp/post/Post.jsx
+++ src/comp/post/Post.jsx
@@ -9,43 +9,21 @@ function Post() {
                 <a href="https://ptfoplayer.github.io/w3/#/post/1">
                 <img className="post-image"
                     src={require("./pobrane.png")}
-                    alt=""
-                />
-                </a>
+                    alt=""/></a>
                 <div className="post-info">
-                    <div className="post-cats">
-                        <span className="post-cat">Leak</span>
-                        <span className="post-cat">News</span>
-                    </div>
                     <a style = {{"textDecoration": "none"}}className="post-title" href="https://ptfoplayer.github.io/w3/#/post/1" >
                         <spam style = {{"textDecoration": "none"}} className = "post-title">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         </spam>
                     </a>
-                    <hr />
                     <span className="post-date">
                         January 1, 2020
                     </span>
+                    <div className="post-cats">
+                        <span className="post-cat">Leak</span>
+                        <span className="post-cat">News</span>
+                    </div>
                 </div>
-                <p className="post-desc">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.
-                </p>
             </div>
         </BrowserView>
         <MobileView>
@@ -53,43 +31,21 @@ function Post() {
                 <a href="https://ptfoplayer.github.io/w3/#/post/1">
                 <img className="post-image"
                     src={require("./pobrane.png")}
-                    alt=""
-                />
-                </a>
+                    alt=""/></a>
                 <div className="post-info">
-                    <div className="post-cats">
-                        <span className="post-cat">Leak</span>
-                        <span className="post-cat">News</span>
-                    </div>
                     <a style = {{"textDecoration": "none"}}className="post-title" href="https://ptfoplayer.github.io/w3/#/post/1" >
                         <spam style = {{"textDecoration": "none"}} className = "post-title">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         </spam>
                     </a>
-                    <hr />
                     <span className="post-date">
                         January 1, 2020
                     </span>
+                    <div className="post-cats">
+                        <span className="post-cat">Leak</span>
+                        <span className="post-cat">News</span>
+                    </div>
                 </div>
-                <p className="post-desc">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam, quidem.
-                </p>
             </div>
         </MobileView>
     </>
