56423aa99fbdce5f0f235f3094141a9b5fddc961
WhiskyAKM <patryk041103@gmail.com>
2022-06-03 15:57:03 +0200

    actual post commited for testing

--- src/comp/SinglePost/SinglePost.jsx
+++ src/comp/SinglePost/SinglePost.jsx
@@ -11,7 +11,7 @@ function SinglePost() {
                     className="Single-Post-img"
                     />
                     <h1 className="Single-Post-title">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        GT/GTX 1630 jednak nie 31 maja?
                         <div className="Single-Post-edit">
                             <i className="Single-Post-icon fa fa-heart"></i>
                         </div>
@@ -21,32 +21,33 @@ function SinglePost() {
                         <span>Data: <b>31 maj</b></span>
                     </div>
                     <p className='Single-Post-desc'>
-                    Dzisiaj mamy 31 maja
-
-Miałą dzisiaj wyjść nowa karta Nvidii: GT/GTX 1630
-
-Jednak dzisiaj jej nie zobaczymy 
-Za to zobaczyliśmy wczoraj przeciek o tym że ta karta wychodzi 15 czerwca
-Przecieki są z różnych źródeł więc możemy powątpiewać czy ta karta wyjdzie, chociaż VideoCardz twierdzi że mają oni tabelę z datami na embargo co do spzedaży i recenzji tej karty
-
+                    Dzisiaj mamy 31 maja, a więc miała dzisiaj wyjść nowa karta Nvidii: GT/GTX 1630.
+</p><p>
+Jednak dzisiaj jej nie zobaczymy, za to zobaczyliśmy wczoraj przeciek o tym że ta karta wychodzi 15 czerwca.
+Przecieki z tymi datami są z różnych źródeł więc możemy powątpiewać czy ta karta wyjdzie, 
+chociaż VideoCardz twierdzi że mają oni tabelę z datami na embargo co do spzedaży i recenzji tej karty.
+</p><p>
 Przypomnę tylko że ta karta ma być wydajnościowo taka sama jak GTX 1050TI tylko ma posiadać pamięć GDDR6 
-
-Jeśli natomiast chodzi o karty serii 4000 
+</p><p>
+Jeśli natomiast chodzi o karty serii 4000.
+</p><p>
 AD104 czyli rdzeń RTX 4070 / 4060TI ma wchodzić w najbliższym czasie do pierwszej fazy testowej czyli umieszczenia rrdzenia na docelowych PCB
-
+</p><p>
 Do tego znamy kolejność wypuszczania grafik serii 4000 
 Najpierw wyjdzie 4090 potem 4080 a następnie 4070 i reszta kart
 Wersje TI będą wychodzić dopiero w roku 2023
-
+</p><p>
 A teraz do intela 
+</p><p>
 13 generacja dostanie jak już wiemy więcej rdzeni bo aż 24 rdzenie i 32 wątki
 Jednak to nie wszystkie upgrady które dostajemy względem 12 gen 
 Zobaczymy tutaj lepszy kontroler pamięci RAM dla pamięci DDR5
 Będzie to 5200Mhz na gear 1 (12 gen posiada 4800Mhz na gear2)
-
+</p><p>
 Natomiast przechodząca do ich grafik to jest duże prawdopodobieństwo że niedługo zobaczymy te karty ponieważ ostatnio Intel popełnił błąd i udostępnił stronę ARC z danymi o kartach ARC A3 (najniższa seria), niestety strona zostałą już zdjęta
-
+</p><p>
 A jeśli chodzi o AMD i ich ryzen 7000
+</p><p>
 Najmocniejszy procesor który udało się wyprodukować AMD z tej generacji pracuje na pojedyńczym rdzeni na 5,8Ghz 
 Co może oznaczać że 5,5Ghz na wszystkich rdzeniach jest bardzo prawdopodobne
                     </p>
