30b8419dd6e739d3f22d84443016616264e4a8af
WhiskyAKM <patryk041103@gmail.com>
2022-06-23 22:33:09 +0200

    improved about page with states

---
+++ src/comp/about/about_data.js
@@ -0,0 +1,23 @@
+function about_data(){
+    return([
+        {
+            title:"Ogółem o nas",
+            paragraph: "Jesteśmy grópą osób interesujących się nowinkami w świecie technologii. Stwozryliśmy ten blog aby dzielić się z ludźmi newsami "+
+            "i przeciekami o których my sami usłyszeliśmy lub/i które znaleźliśmy."
+        },
+        {
+            title:"O piszących",
+            paragraph: "Patryk 'Whisky/PTFOPlayer' to główny piszący oraz twórca 'Technews'."
+        },
+        {
+            title: "O osobie odpowiedzialnej za kanał na telegramie",
+            paragraph: "Michał 'Wloseczeq' odpowiada za nasz kanał na Telegramie, co prawda jest on tylko i wyłącznie moderatorem lecz bez jego pomocy 'Technews' nie był by tak rozwinięty"
+        },
+        {
+            title: "O tym jak utrzymujemy gramatyke na poziomie",
+            paragraph: "Martyna'Martirza' może i nie zainteresowana technologią i nowinkami w tej dziedzinie ale pomaga popzez sprawdzanie ortografii, gramatyki oraz interpunkcji w naszych postach."
+        }
+    ])
+}
+
+export default about_data
